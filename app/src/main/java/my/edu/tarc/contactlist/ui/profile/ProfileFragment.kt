f577c30925acc6bfe74e4a8d20e5317be7e11052
AngelCham09 <angelchamkailing@gmail.com>
2022-02-18 22:27:11 +0800

    Initial

---
+++ app/src/main/java/my/edu/tarc/contactlist/ui/profile/ProfileFragment.kt
@@ -0,0 +1,54 @@
+package my.edu.tarc.contactlist.ui.profile
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import my.edu.tarc.contactlist.R
+import my.edu.tarc.contactlist.databinding.FragmentGalleryBinding
+import my.edu.tarc.contactlist.databinding.FragmentProfileBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ProfileFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!  //when binding value available, i want get if
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val editTextPersonName: EditText = view.findViewById(R.id.editTextTextPersonName)
+        val editTextPhone: EditText = view.findViewById(R.id.editTextPhone)
+        val buttonSave: Button = view.findViewById(R.id.buttonSave)
+
+        buttonSave.setOnClickListener{
+            Toast.makeText(context,"Record saved", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.findItem(R.id.action_settings).setVisible(false)
+    }
+}
